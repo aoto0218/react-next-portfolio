b90aabe4227f271f5b18b0b9163a312a69ac5d2a
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2025-01-22 16:18:46 +0900

    ヘッダーコンポーネントにハンバーガーメニューを追加し、スタイルを調整。ファイルリンクのホバー効果を追加。レスポンシブデザインのための幅のトランジションを設定。

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./index.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Header(){
-    return(
+export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
         <header className={styles.header}>
             <Link href="/">
                 <Image
@@ -15,20 +24,36 @@ export default function Header(){
                 />
             </Link>
             <p className={styles.name}>早苗 碧大</p>
-            <nav>
+            <button
+                className={`${styles.hamburger} ${isMenuOpen ? styles.open : ""}`}
+                onClick={toggleMenu}
+                aria-label="Toggle navigation"
+            >
+                ☰
+            </button>
+            <nav className={`${styles.nav_container} ${isMenuOpen ? styles.show : ""}`}>
                 <ul className={styles.nav}>
                     <li className={styles.nav_item}>
-                        <Link href="/" className={styles.nav_link}><p className={styles.mini}>ホーム</p><p>Home</p></Link>
+                        <Link href="/" className={styles.nav_link}>
+                            <p className={styles.mini}>ホーム</p>
+                            <p>Home</p>
+                        </Link>
                     </li>
                     <li className={styles.nav_item}>
-                        <Link href="/qualifications" className={styles.nav_link}><p className={styles.mini}>資格</p><p>Qualifications</p></Link>
+                        <Link href="/qualifications" className={styles.nav_link}>
+                            <p className={styles.mini}>資格</p>
+                            <p>Qualifications</p>
+                        </Link>
                     </li>
                     <li className={styles.nav_item}>
-                        <Link href="/deliverables" className={styles.nav_link}><p className={styles.mini}>成果物</p><p>Deliverables</p></Link>
+                        <Link href="/deliverables" className={styles.nav_link}>
+                            <p className={styles.mini}>成果物</p>
+                            <p>Deliverables</p>
+                        </Link>
                     </li>
                 </ul>
             </nav>
             <div className={styles.gradient}></div>
         </header>
-    )
+    );
 }
